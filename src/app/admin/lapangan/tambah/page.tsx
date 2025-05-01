41a4656f71abf4437f44e2201ce878264c40affe
Azzaxy <abdurrohmanazis@gmail.com>
2025-05-01 23:10:25 +0700

    feat: refactor AddCourtPage and AddOrderPage; replace custom back button implementation with BackButton component

--- src/app/admin/lapangan/tambah/page.tsx
+++ src/app/admin/lapangan/tambah/page.tsx
@@ -1,23 +1,10 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { ArrowLeft } from "lucide-react";
-import { useRouter } from "next/navigation";
 import CourtForm from "@/components/Admin/Court/court-form";
+import BackButton from "@/components/BackButton";
 
 const AddCourtPage = () => {
-  const router = useRouter();
-
   return (
     <div className="container  mx-auto pb-8">
-      <Button
-        variant="outline"
-        className="mb-6 gap-2"
-        onClick={() => router.back()}
-      >
-        <ArrowLeft size={16} />
-        Kembali
-      </Button>
+      <BackButton />
 
       <CourtForm isAddForm />
     </div>
--- src/app/admin/pemesanan/tambah/page.tsx
+++ src/app/admin/pemesanan/tambah/page.tsx
@@ -1,23 +1,10 @@
-"use client";
-
-import { Button } from "@/components/ui/button";
-import { ArrowLeft } from "lucide-react";
-import { useRouter } from "next/navigation";
 import OrderForm from "@/components/Admin/Pemesanan/order-form";
+import BackButton from "@/components/BackButton";
 
 const AddOrderPage = () => {
-  const router = useRouter();
-
   return (
     <div className="container  mx-auto pb-8">
-      <Button
-        variant="outline"
-        className="mb-6 gap-2"
-        onClick={() => router.back()}
-      >
-        <ArrowLeft size={16} />
-        Kembali
-      </Button>
+      <BackButton />
 
       <OrderForm isAddForm />
     </div>
