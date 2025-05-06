ed9bd382a83e68b396d70289a6d8c1431fb8886f
Azzaxy <abdurrohmanazis@gmail.com>
2025-05-06 21:25:15 +0700

    feat: refactor EditProfileForm to  Input and Label component shadcn/ui for improved consistency

--- src/components/Profile/edit-profile/index.tsx
+++ src/components/Profile/edit-profile/index.tsx
@@ -2,6 +2,8 @@
 
 import { Button } from "@/components/ui/button";
 import { CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { useSession } from "next-auth/react";
 import React from "react";
 import toast from "react-hot-toast";
@@ -20,16 +22,16 @@ const EditProfileForm = () => {
       <form onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="space-y-2">
-            <label className="text-sm font-medium">Nama Lengkap</label>
-            <input
+            <Label className="text-sm font-medium">Nama Lengkap</Label>
+            <Input
               type="text"
               className="w-full p-2 border rounded-md"
               defaultValue={user?.name || ""}
             />
           </div>
           <div className="space-y-2">
-            <label className="text-sm font-medium">Email</label>
-            <input
+            <Label className="text-sm font-medium">Email</Label>
+            <Input
               type="email"
               className="w-full p-2 border rounded-md"
               defaultValue={user?.email || ""}
@@ -37,8 +39,8 @@ const EditProfileForm = () => {
           </div>
           {user?.phone && (
             <div className="space-y-2">
-              <label className="text-sm font-medium">Nomor Telepon</label>
-              <input
+              <Label className="text-sm font-medium">Nomor Telepon</Label>
+              <Input
                 type="tel"
                 className="w-full p-2 border rounded-md"
                 defaultValue={user?.phone || ""}
