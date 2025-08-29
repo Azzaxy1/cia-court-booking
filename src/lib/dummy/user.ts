0efc5cf16a70160efd695a00e88947a388115602
Azzaxy <abdurrohmanazis@gmail.com>
2025-08-29 19:15:52 +0700

    refactor: remove unused dummy data files for courts, orders, and user

--- src/lib/dummy/user.ts
+++
@@ -1,7 +0,0 @@
-// Data dummy untuk user
-export const user = {
-  name: "Abdurrohman Azis",
-  email: "abdurrohmanazis@email.com",
-  phone: "081234567890",
-  image: null, // Ganti dengan URL foto profil jika ada
-};
